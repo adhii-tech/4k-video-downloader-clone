df334f0d73d437e009d17690a776a64506de1cc9
adhii-tech <adithyanb802@gmail.com>
2026-07-21 01:19:19 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,23 +5,34 @@ import vue from "@vitejs/plugin-vue";
 export default defineConfig({
   plugins: [vue()],
 
-  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-  // prevent vite from obscuring rust errors
+  // Prevent Vite from clearing the terminal
   clearScreen: false,
-  // tauri expects a fixed port, fail if that port is not available
+
   server: {
     port: 1420,
     strictPort: true,
+
+    watch: {
+      ignored: [
+        "**/src-tauri/target/**",
+        "**/src-tauri/target/**/*",
+        "**/target/**",
+        "**/target/**/*",
+      ],
+    },
   },
-  // to make use of `TAURI_DEBUG` and other env variables
-  // https://tauri.studio/v1/api/config#buildconfig.beforedevcommand
+
+  // Tauri environment variables
   envPrefix: ["VITE_", "TAURI_"],
+
   build: {
-    // Tauri supports es2021
+    // Tauri supports ES2021
     target: ["es2021", "chrome100", "safari13"],
-    // don't minify for debug builds
+
+    // Don't minify during debug
     minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
-    // produce sourcemaps for debug builds
+
+    // Generate sourcemaps in debug mode
     sourcemap: !!process.env.TAURI_DEBUG,
   },
 });
